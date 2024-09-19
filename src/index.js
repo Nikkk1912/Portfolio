eed39db35c1cb15ee92821d9f5292af9a4e8fd63
Nikkk1912 <moskalevskiy1@gmail.com>
2024-09-20 01:30:36 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const navMenu = document.querySelectorAll('.nav-sections');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem("theme", "light");
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem("theme", "light");
+    }
+}
+
+toggleSwitch.addEventListener('change',switchTheme);
+
+const currentTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+    }
+}
