df4425f2ecaae7604964ca3339779cc32db82077
Nikkk1912 <moskalevskiy1@gmail.com>
2024-09-20 01:44:49 +0300

    user theme preference save

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem("theme", "light");
+        localStorage.setItem("theme", "dark");
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem("theme", "light");
