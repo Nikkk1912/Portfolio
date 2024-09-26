a2ebac19ca9cdba8d6bed1819ed6bf99051ed752
Nikkk <moskalevskiy2@gmail.com>
2024-09-26 22:08:15 +0300

    input form implementation

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
+const emailHandler = require('./email-handler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the assets and src directories
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 app.use('/', express.static(path.join(__dirname, '../src')));
 
@@ -22,6 +25,9 @@ app.get('/index_main.html', (req, res) => {
 app.get('/index_socials.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/html/output/index_socials.html'));
 });
+
+// Post endpoint to save email from input form
+app.post('/submit-email', emailHandler.handleEmailSubmission)
 
 
 app.listen(PORT, () => {
